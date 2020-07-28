78d63f271835654aeb80d042e0bc995822e2c740
Filipe Soccol <filipe.soccol@gmail.com>
2020-07-27 23:04:58 -0300

    Changed deploy paths

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   chainWebpack: config => config.resolve.symlinks(false),
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/ipfs-vue/'
+    ? '/ipfs-pubsub-vue/'
     : '/'
 }
